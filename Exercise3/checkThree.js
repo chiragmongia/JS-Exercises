957d6d63b753a544979fae8044477a339c113571
Chirag Mongia <chirag.mongia@vinsol.com>
2013-10-17 14:40:22 +0530

    removed console.log statements

--- Exercise3/checkThree.js
+++ Exercise3/checkThree.js
@@ -9,14 +9,10 @@ for (var i=0; i < days.length; i++) {
       none.checked = false;
       if (this.checked) {
         selectedDays.push(this);
-        console.log(selectedDays);
-        console.log(selectedDays.length);
       }
 
       else if(this.checked == false) {
         selectedDays.splice(selectedDays.indexOf(this), 1);
-        console.log(selectedDays);
-        console.log(selectedDays.length);
       }
 
       if (selectedDays.length > maxDays) {
@@ -32,9 +28,7 @@ var checkNone = function() {
   for(var i=0; i < days.length; i++) {
     if (none.checked == true) {
       selectedDays = [];
-      console.log(days[i]);
       days[i].checked = false;
-      console.log(selectedDays);
     }
   }
 }
