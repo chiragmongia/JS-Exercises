425294db0fc390265c5447febcef65e564012403
Chirag Mongia <chirag.mongia@vinsol.com>
2013-11-03 15:48:15 +0530

    adding exercise: Welcome user

---
+++ welcomeUser/js/welcomeUser.js
@@ -0,0 +1,36 @@
+var welcomeUser = function() {
+  this.init();
+}
+
+welcomeUser.prototype = {
+  init: function() {
+    this.firstName = prompt("Enter First Name");
+    this.lastName = prompt("Enter Last Name");
+    this.valiateName();
+  },
+
+  valiateName: function() {
+    var obj = this;
+    if (obj.firstName != null && obj.lastName != null) {
+      obj.firstName = obj.firstName.replace(/(^\s+|\s+$)/g,'');
+      obj.lastName = obj.lastName.replace(/(^\s+|\s+$)/g,'');
+    }
+
+    if (obj.firstName && obj.lastName) {
+      alert("Hello " + obj.firstName + " " + obj.lastName + ".");
+      var welcomeHeading = document.createElement("h1");
+      var headingText = document.createTextNode("Hello " + obj.firstName + " " + obj.lastName + ".");
+      welcomeHeading.appendChild(headingText);
+      document.body.appendChild(welcomeHeading);
+    }
+
+    else {
+      var defaultHeading = document.createElement("h1");
+      var defaultText = document.createTextNode("Name was not entered correctly.");
+      defaultHeading.appendChild(defaultText);
+      document.body.appendChild(defaultHeading);
+    }
+  }
+}
+
+var obj = new welcomeUser();
