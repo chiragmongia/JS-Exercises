07d4ab767cafd06634361e6c5c7264fc4b7c93d4
Chirag Mongia <chirag.mongia@vinsol.com>
2013-11-11 14:34:45 +0530

    modified email regex, separated regex from class and optimized code

--- emailUrlFormatValidation/js/emailUrlFormatValidation.js
+++ emailUrlFormatValidation/js/emailUrlFormatValidation.js
@@ -1,57 +1,43 @@
-var registrationform = function() {
+emailPattern = /^\w+.?\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/i;  //Very basic regex for email
+urlPattern = /((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i ;
+
+var Registrationform = function() {
   this.init();
 }
 
-registrationform.prototype = {
+Registrationform.prototype = {
   init: function() {
     this.form = document.getElementById("regForm");
     this.email = document.getElementById("email");
     this.url = document.getElementById("homepage");
-    this.emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$/;  //Very basic regex for email
-    this.urlPattern = /((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i ;
     this.validateForm();
   },
 
   validateForm: function() {
     var obj = this;
     obj.form.onsubmit = function() {
-      if (obj.validateEmailFormat() && obj.validateUrlFormat()) {
+      if (obj.validateEmailAndUrl(obj.email, emailPattern) && obj.validateEmailAndUrl(obj.url, urlPattern)) {
         return true;
       }
       return false;
     }
   },
 
-  validateEmailFormat: function() {
+
+  validateEmailAndUrl: function(inputField, pattern) {
     var obj = this;
-    if (email.value == "") {
-      alert("Email cannot be blank")
-      email.focus();
+    if (inputField.value == "") {
+      alert(inputField.name + " cannot be blank")
+      inputField.focus();
       return false;
     }
-    else if (!obj.emailPattern.test(email.value)) {
-      alert("Please enter a valid email");
-      email.focus();
+    else if (!pattern.test(inputField.value)) {
+      alert("Please enter a valid " + inputField.name);
+      inputField.focus();
       return false;
     }
     return true;
   },
-
-  validateUrlFormat: function() {
-    var obj = this;
-    if (obj.url.value == "") {
-      alert("Homepage cannot be blank");
-      obj.url.focus();
-      return false;
-    }
-
-    else if (!obj.urlPattern.test(obj.url.value)) {
-      alert("Please enter a valid url in Homepage");
-      obj.url.focus();
-      return false;
-    }
-    return true;
-  }
 }
 
-var form = new registrationform();
+var form = new Registrationform();
