e3e98b6fa4f09f57cda729c32581e2b2c373fa53
Chirag Mongia <chirag.mongia@vinsol.com>
2013-11-18 00:19:13 +0530

    changed onclick to addEventListener

--- checkAllcheckNone/js/checkAllcheckNone.js
+++ checkAllcheckNone/js/checkAllcheckNone.js
@@ -17,17 +17,17 @@ colors.prototype = {
   BindEventForCheckAll: function() {
     var obj = this;
     var checkAllbtn = document.getElementById("all");
-    checkAllbtn.onclick = function() {
-      obj.changeStateForCheckboxes(true);
-    }
+    checkAllbtn.addEventListener("click", function() {
+      obj.changeStateForCheckboxes(true)
+    });
   },
 
   BindEventForCheckNone: function() {
     var obj = this;
     var checkNoneBtn = document.getElementById("none");
-    checkNoneBtn.onclick = function() {
+    checkNoneBtn.addEventListener("click", function() {
       obj.changeStateForCheckboxes(false);
-    }
+    });
   }
 }
 
