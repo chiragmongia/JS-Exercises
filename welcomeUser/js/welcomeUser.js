6a89b06342e134660b6f30d87d0ca160267f34bf
Chirag Mongia <chirag.mongia@vinsol.com>
2013-11-20 19:28:39 +0530

    changed class name. Code refactored

--- welcomeUser/js/welcomeUser.js
+++ welcomeUser/js/welcomeUser.js
@@ -1,36 +1,41 @@
-var welcomeUser = function() {
+var User = function() {
   this.init();
 }
 
-welcomeUser.prototype = {
+User.prototype = {
+  firstName: null,
+  lastName: null,
+  inputVal: null,
+
   init: function() {
-    this.firstName = prompt("Enter First Name");
-    this.lastName = prompt("Enter Last Name");
-    this.valiateName();
+    // this.inputVal;
+    this.readNameAndTrim('first');
+    this.readNameAndTrim('last');
+    this.displayNameInDocument();
   },
 
-  valiateName: function() {
-    var obj = this;
-    if (obj.firstName != null && obj.lastName != null) {
-      obj.firstName = obj.firstName.trim();
-      obj.lastName = obj.lastName.trim();
-    }
+  readNameAndTrim: function(type) {
+    promptString = "Enter " + type + " name";
+    this.inputVal = (prompt(promptString) || "").trim();
+    this.validateName(type);
+  },
 
-    if (obj.firstName && obj.lastName) {
-      alert("Hello " + obj.firstName + " " + obj.lastName + ".");
-      var welcomeHeading = document.createElement("h1");
-      var headingText = document.createTextNode("Hello " + obj.firstName + " " + obj.lastName + ".");
-      welcomeHeading.appendChild(headingText);
-      document.body.appendChild(welcomeHeading);
+  validateName: function(type) {
+    alertString = type + " name can't be blank";
+    while(!this.inputVal) {
+      alert(alertString);
+      this.readNameAndTrim(type);
     }
+    this[type + 'Name'] = this.inputVal;
+  },
 
-    else {
-      var defaultHeading = document.createElement("h1");
-      var defaultText = document.createTextNode("Name was not entered correctly.");
-      defaultHeading.appendChild(defaultText);
-      document.body.appendChild(defaultHeading);
-    }
+  displayNameInDocument: function() {
+    alert("Hello " + this.firstName + " " + this.lastName + ".");
+    var welcomeHeading = document.createElement("h1");
+    var headingText = document.createTextNode("Hello " + this.firstName + " " + this.lastName + ".");
+    welcomeHeading.appendChild(headingText);
+    document.body.appendChild(welcomeHeading);
   }
 }
 
-var welcomeUser = new welcomeUser();
+var welcomeUser = new User();
